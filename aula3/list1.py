2a82f422201154ac96a5b784381a268372d521f0
ansattz <dvieira@disroot.org>
2021-07-23 22:40:45 -0300

    att

---
+++ aula3/list1.py
@@ -0,0 +1,30 @@
+""" Primeira lista de exercícios que foi proposta no endereço machineteachin.tech 
+para a disciplina Computação I """
+# Exercício 1.
+# Número de bombons com input sendo o quanto de dinheiro possui para comprar(x) e o preço do bombom(y).
+# def num_bombons(x,y):
+#    try:
+#       qbombons=int(x/y)
+#       print(qbombons)
+#    except:
+#       print("Você pode pegar os bombons de graça.")
+
+# num_bombons(50,0)
+
+
+# Exercício 2.
+# Quantos carros necessário para a viagem com input sendo número de pessoas(x) e capacidade de carros não convencionais(y).
+# def carros(x,y):
+#    ppl_pla=int(x/5)
+#    nconv_cars_pla=int(int(x%5)/y)
+#    return ppl_pla + nconv_cars_pla
+
+
+# Exercício 3.
+# Quantos bolos o rapaz consegue fazer com os ingredientes.
+# def bolos(x,y,z):
+#     i1=x/2
+#     i2=y/3
+#     i3=z/5
+#     qtbolo=int((i1 + i2 + i3)/3)
+#     return qtbolo
---
+++ aula3/list2.py
@@ -0,0 +1,49 @@
+""" Segunda lista de exercícios proposta no endereço machineteachin.tech
+para a disciplina de Computação I """
+# Exercício 1.
+# Teste da tricotomia. A função retorna uma resposta dizendo se um número é positivo, negativo ou zero.
+# def PosNegZero(x):
+#     if x>0:
+#         return str(x) + " positivo"
+#     elif x<0:
+#         return str(x) + " negativo"
+#     else:
+#         return str(x) + " zero"
+
+# print(PosNegZero(-1))
+
+
+# Exercício 2.
+# Classificação por pontuação com três entradas por time.
+# def classificacao(cv, ce, cs, fv, fe, fs):
+#     flamengo1=3*cv+ce
+#     fluminense1=3*fv+fe
+#     flamengo2=3*cv+ce+cs
+#     fluminense2=3*fv+fe+fs    
+#     if flamengo1>fluminense1:
+#         return "Flamengo"
+#     elif fluminense1>flamengo1:
+#         return "Fluminense"
+#     elif fluminense2>flamengo2:
+#         return "Fluminense"
+#     elif flamengo2>fluminense2:
+#         return "Flamengo"
+#     else:
+#         return "Empate"
+
+# print(classificacao(9,5,-1,10,2,10))
+
+
+# Exercício 3.
+# Possibilidade da competição acontecer se houve papeis suficientes para os competidores.
+# def avioes(c, p_compr, p_compet):
+#     papelpc=c*p_compet
+#     if papelpc>p_compr:
+#         return "Insuficiente"
+#     elif papelpc<p_compr:
+#         return "Suficiente"
+#     else:
+#         return "Suficiente"
+
+# print(avioes(5,40,2))
+
